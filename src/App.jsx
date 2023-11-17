bc5728831980fb4da44450f136ad46c7b12751b6
rpatil <rpatil@business.com>
2023-11-17 23:34:15 +0530

    added random pass generatr funs

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,152 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// const funs = [
+//   smallCharGenerator(),
+//   largeCharGenerator(),
+//   symbolGenerator(),
+//   numGenerator(),
+// ];
+// const numbers=[0,1,2,3,4,5,6,7,8,9]
+// const symbols= ["!","~","@","#","&"];
 
+// let pass=[]
+// let someRandomNum=0
+
+// function smallCharGenerator()
+// {
+//   const someRandom=((Math.random()*(122-97))+97);
+//   return String.fromCharCode(someRandom);
+// }
+// function largeCharGenerator()
+// {
+//   const someRandom = ((Math.random() * (90 - 65)) + 65);
+//   return String.fromCharCode(someRandom);
+// }
+// function symbolGenerator()
+// {
+//   const someRandom=((Math.random()*(symbols.size-0+1))+0);
+//   return someRandom;
+// }
+// function numGenerator()
+// {
+//   const someRandom =(( Math.random() * (numbers.size - 0 + 1)) + 0);
+//   return someRandom;
+// }
+
+// const someRandome=0;
+// function generateRandomPass(size=8,isSymbol=false,isNum=false)
+// {
+//     someRandom = ((Math.random() * (funs.size-0))+0);
+//     pass.push(someRandome);
+//   // for(let i=0;i<size;i++)
+//   // {
+//   //   someRandom = ((Math.random() * (funs.size-0))+0);
+//   //   pass.push(someRandome);
+//   // }
+//   // if(isSymbol && isNum)
+//   // {
+//   // }
+//   // else if(isSymbol)
+//   // {
+//   // }
+//   // else if(isNum)
+//   // {
+//   // }
+// }
+
+function App() {
+  
+const funs = [
+  smallCharGenerator(),
+  largeCharGenerator(),
+  symbolGenerator(),
+  numGenerator(),
+];
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const symbols = ["!", "~", "@", "#", "&"];
+
+let pass = [];
+let someRandomNum = 0;
+
+function smallCharGenerator() {
+  const someRandom = Math.random() * (122 - 97) + 97;
+  return String.fromCharCode(someRandom);
+}
+function largeCharGenerator() {
+  const someRandom = Math.random() * (90 - 65) + 65;
+  return String.fromCharCode(someRandom);
+}
+function symbolGenerator() {
+  const someRandom = Math.random() * (symbols.size - 0 + 1) + 0;
+  return someRandom;
+}
+function numGenerator() {
+  const someRandom = Math.random() * (numbers.size - 0 + 1) + 0;
+  return someRandom;
+}
+
+const someRandome = 0;
+function generateRandomPass(size = 8, isSymbol = false, isNum = false) {
+  someRandom = Math.random() * (funs.size - 0) + 0;
+  pass.push(someRandome);
+  // for(let i=0;i<size;i++)
+  // {
+  //   someRandom = ((Math.random() * (funs.size-0))+0);
+  //   pass.push(someRandome);
+  // }
+  // if(isSymbol && isNum)
+  // {
+  // }
+  // else if(isSymbol)
+  // {
+  // }
+  // else if(isNum)
+  // {
+  // }
+}
+
+
+  let [passValue, generatePass]= useState('Efef23WE#5sd');
+
+  console.log(generateRandomPass());
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="passGenMainContainer">
+        <div>
+          <div>
+            <input type="text" name="pass" value="{}" />
+            <button>copy</button>
+          </div>
+          <div>
+            <input
+              type="range"
+              min="1"
+              max="100"
+              value="50"
+              class="slider"
+              id="passLen"
+            />
+            <h4>Length{}</h4>
+            <input
+              type="checkbox"
+              id="numbers"
+              name="numbers"
+              value="numbers"
+            />
+            <label for="numbers">Numbers</label>
+            <input
+              type="checkbox"
+              id="symbols"
+              name="symbols"
+              value="symbols"
+            />
+            <label for="symbols">Symbols</label>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
