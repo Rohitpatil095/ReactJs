286a6b3cce2eb2a030988d89698d4b8c50080215
rpatil <rpatil@business.com>
2023-11-17 18:27:21 +0530

    used react hook name useState to update var in all places at once without multiple ref

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,44 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+// @V1 - simplying ref var. var is getting updated but react not refreshing var after update  
+  // let value=21;
+
+  // const add=()=>{
+  //   value+=1;
+  // };
   
-  let value=21;
-  
-  const add=()=>{
-    value+=1;
-  };
-  
-  const sub=()=>
-  {
-    value-=1;
+  // const sub=()=>
+  // {
+  //   value-=1;
+  // }
+
+// ----------------------------------
+
+  let [value, updateIt] = useState(21);
+
+  const add= () =>{
+    updateIt(value+1);
   }
+
+    const sub = () => {
+      updateIt(value - 1);
+    };
 
   return (
     <>
       <h1>Lets Code Counter...</h1>
 
+      {/* @V1 */}
+      {/* <div className="counterContainer">
+        <h2>Value is {value}</h2>
+        <button onClick={add}>+1</button>
+        <br />
+        <button onClick={sub}>-1</button>
+      </div> */}
+
+{/* ------------------- */}
       <div className="counterContainer">
         <h2>Value is {value}</h2>
         <button onClick={add}>+1</button>
