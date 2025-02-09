e41f637c6aeff53ed19a00b8c30fa3fb33f50d4d
Rohitpatil095 <rohitpatil1340sa@gmail.com>
2025-02-09 12:40:32 +0530

    [update] user schemavalidation

--- LearningProjects/paytm/paytm-backend/models/userSchema.js
+++ LearningProjects/paytm/paytm-backend/models/userSchema.js
@@ -1,14 +1,45 @@
-const mongoose =require("mongoose");
+const mongoose = require("mongoose");
 
-const user=new mongoose.Schema(
-    {
-        userName: String,
-        password: String,
-        email: String
-    }
-)
+const user = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: [true, "username is required"],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 2,
+    maxLength: 36,
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+    minLength: 8,
+  },
+  firstName: {
+    type: String,
+    minLength: 2,
+    maxLength: 50,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    minLength: 2,
+    maxLength: 50,
+    trim: true,
+  },
+  email: {
+    type: String,
+    minLength: 2,
+    maxLength: 50,
+    trim: true,
+    unique: true,
+    required: [true, "user email is required"],
+    validate: {
+      validator: function (validate) {
+        return /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/i.test(validate);
+      },
+    },
+  },
+});
 
-const userModel = mongoose.model("user", user);
-
-
-module.exports = { user, userModel };
+module.exports = { user };
