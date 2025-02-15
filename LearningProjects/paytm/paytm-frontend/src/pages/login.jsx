a869b2ab55494f01071464a09ff6de09b62e1b43
Rohitpatil095 <rohitpatil1340sa@gmail.com>
2025-02-15 11:37:39 +0530

    paytm pages added- shell pages

---
+++ LearningProjects/paytm/paytm-frontend/src/pages/login.jsx
@@ -0,0 +1,9 @@
+export function Login(){
+    return(<>
+        login header
+        email
+        password
+        button login
+        dont had account? signup {link to signup page} 
+    </>)
+}
